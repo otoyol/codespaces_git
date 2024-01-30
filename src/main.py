dacd1e5c21f998703f64c5cdcabb3ec274bb381f
otoyol <33404822+otoyol@users.noreply.github.com>
2024-01-30 01:32:12 +0000

    Update main.py
    fixed typo

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 if __name__ == "__main__":
     current_datetime = datetime.now().strftime("%d-%m-%Y %H:%M")
-    message = f"Congratulations, you are running the a python script from a GitHub Codespace environment. Now, it is {current_datetime} and time for further development."
+    message = f"Congratulations, you are running a python script from a GitHub Codespace environment. Now, it is {current_datetime} and time for further development."
     
     print(message)
